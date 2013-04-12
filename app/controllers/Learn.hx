529e32538b5be3d703b41c5f23c70cae24c213f0
Matt Tuttle <heardtheword@gmail.com>
2013-04-12 07:37:43 -0500

    improved learn view

--- app/controllers/Learn.hx
+++ app/controllers/Learn.hx
@@ -10,9 +10,35 @@ class Learn extends core.Controller
 	{
 		var tutorials = db.posts.find({
 			publish_ts: { '$lt': Date.now() }
+		}, ["id", "title", "slug", "category"]).sort({
+			publish_ts: 1
+		}).results();
+
+		var categories = new Array<String>();
+		for (tutorial in tutorials)
+		{
+			if (Lambda.indexOf(categories, tutorial.category) == -1)
+			{
+				categories.push(tutorial.category);
+			}
+		}
+		categories.sort(function(a:String, b:String):Int {
+			return a < b ? -1 : 1;
+		});
+
+		view("learn/home", {
+			categories: categories,
+			tutorials: tutorials
+		});
+	}
+
+	public function tutorial(slug:String)
+	{
+		var tutorial = db.posts.findOne({
+			slug: slug
 		}, ["id", "title", "slug", "content"]);
 
-		view("learn/list", { tutorials: tutorials.results() });
+		view("learn/view", { tutorial: tutorial });
 	}
 
 	public function feed()
